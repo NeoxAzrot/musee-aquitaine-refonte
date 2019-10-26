1b86bae3ba56c8edc7ffbff0e662c053d02d7ab4
NeoxAzrot1 <sami.lepays@gmail.com>
2019-10-26 11:47:17 +0200

    Agenda

--- assets/js/arrow_animation.js
+++ assets/js/arrow_animation.js
@@ -13,3 +13,20 @@ $("#slide_right").bind("webkitAnimationEnd mozAnimationEnd animationend", functi
 $("#slide_right").hover(function(){
   $(this).addClass("ricochet_right");        
 })
+
+
+$("#agenda_left").bind("webkitAnimationEnd mozAnimationEnd animationend", function(){
+  $(this).removeClass("ricochet_agenda_left")  
+})
+
+$("#agenda_left").hover(function(){
+  $(this).addClass("ricochet_agenda_left");        
+})
+
+$("#agenda_right").bind("webkitAnimationEnd mozAnimationEnd animationend", function(){
+  $(this).removeClass("ricochet_agenda_right")  
+})
+
+$("#agenda_right").hover(function(){
+  $(this).addClass("ricochet_agenda_right");        
+})
